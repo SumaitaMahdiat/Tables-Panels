a0aaa002ee3de019a931a23780d97bed82c3776f
AtikaTabassum <115262496+AtikaTabassum@users.noreply.github.com>
2024-12-31 12:18:29 +0600

    Add files via upload

---
+++ Atika/search.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('dbconnection.php');
+
+if(isset($_GET['search'])){
+    $searchName = $_GET['search'];
+   
+
+    $sql = "SELECT * FROM manga WHERE Name Like %$searchName$ ";
+
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0){
+        // echo "Let him enter";
+        while ($row = $result->fetch_assoc())
+          echo "<li><strong>Name:</strong></li>";
+    }
+    else{
+        echo "Title not found.";
+    }
+}
+
