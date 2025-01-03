438ebee132e75e2bce141c6439f3bfe4213be588
AtikaTabassum <115262496+AtikaTabassum@users.noreply.github.com>
2025-01-03 19:53:51 +0600

    Update signup.php

--- project/signup.php
+++ project/signup.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <main>
         <section class="login">
             <div class="login_box">
                 <h1>Sign Up</h1>
-        <form class="login_form" action="sign_up_process.php" method="post">
+        <form class="login_form" action="signup_process.php" method="post">
             <input
               type="text"
               id="username"
