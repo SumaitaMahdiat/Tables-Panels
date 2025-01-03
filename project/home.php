6ed40f1d69d4f56fcf13a168f921485f0e650e20
AtikaTabassum <115262496+AtikaTabassum@users.noreply.github.com>
2025-01-04 02:29:31 +0600

    Update home.php

--- project/home.php
+++ project/home.php
@@ -9,7 +9,7 @@ $manga = "SELECT Name, Genre, Covers,Author, Chapters FROM manga ORDER BY ID";
 $res1 = mysqli_query($conn, $manga);
 $res2 = mysqli_query($conn, $novel);
 $mangaList = mysqli_fetch_all($res1, MYSQLI_ASSOC);
-$novellist  =  mysqli_fetch_all($res2, MYSQLI_ASSOC);
+$novelList  =  mysqli_fetch_all($res2, MYSQLI_ASSOC);
 mysqli_free_result($res1);
 mysqli_free_result($res2);
 
@@ -47,16 +47,20 @@ mysqli_free_result($res2);
     </div>
 
 <h1 style="text-align: center; padding: 20px;">Webnovel Gallery</h1>
-    <div class="gallery">
-        <?php foreach ($novellist as $novel): ?>
+<div class="gallery">
+        <?php foreach ($novelList as $novel): ?>
             <div class="card">
-            <?php echo "<img src='$novelimageUrl'>"; ?>
+            
+            <?php   $novel_name = $novel['Name'];
+                    include('image.php'); ?>
                 <h3><a href="novel_page.php?Name=<?php echo urlencode($novel['Name']); ?>">
-                            <strong><?php echo htmlspecialchars($novel['Name']); ?></strong></a></h3>
+                            <strong><?php echo htmlspecialchars($novel['Name']); ?></strong>
+                        </a></h3>
                 <p>Author: <?php echo htmlspecialchars($novel['Author']); ?></p>
                 <p>Genre:<?php echo htmlspecialchars($novel['Genre']); ?></p>
                 <p><?php echo htmlspecialchars($novel['Chapters']); ?> Chapters</p>
             </div>
+           
         <?php endforeach; ?>
     </div>
     <?php include('footer.php'); ?>
