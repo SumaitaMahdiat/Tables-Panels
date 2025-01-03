851e83726755b29e0d43eeaf4cd55453f9f15e08
sanchita-sarker <112814844+sanchita-sarker@users.noreply.github.com>
2025-01-03 17:03:47 +0600

    Update search.php

--- Puja/search.php
+++ Puja/search.php
@@ -9,44 +9,7 @@
     </head>
     <body>
 
-    <!-- Header -->
-    <div class="header">
-        <h1>TALES & PANELS</h1>
-        <p>A getaway to the world of manga and novels</p>
-    </div>
-
-    <!-- Navbar -->
-    <div class="navbar">
-        <a href="home.php">Home</a>
-        <div class="dropdown">
-            <button class="dropbtn">Genres</button>
-            <div class="dropdown-content">
-                <a href="genre_pg.php?genre=Fantasy">Fantasy</a>
-                <a href="genre_pg.php?genre=Comedy">Comedy</a>
-                <a href="genre_pg.php?genre=Sports">Sports</a>
-                <a href="genre_pg.php?genre=Romance">Romance</a>
-                <a href="genre_pg.php?genre=Action">Action</a>
-                <a href="genre_pg.php?genre=Horror">Horror</a>
-            </div>
-        </div>
-        <div class="dropdown">
-      <button class="dropbtn">My Bookmarks</button>
-      <div class="dropdown-content">
-        <a href="bookmarked_manga_pg.html">Manga</a>
-        <a href="bookmarked_novel_pg.html">Novel</a>       
-      </div>
-    </div>
-        <a href="logout.php" class="right">Log Out</a>
-
-        <div class="search-container">
-            <form action="search.php" method="GET">
-                <input type="text" placeholder="Search for Manga.." name="query" required>
-                <button type="submit">Go</button>
-            </form>
-        </div>
-    </div>
-
-
+    <?php require_once("header.php"); ?>
 <?php
 // Include the database connection file
 require_once('dbconnection.php');
@@ -87,4 +50,5 @@ if (isset($_GET['query'])) {
     echo "Please enter a search query.";
 }
 ?>
+<?php include('footer.php'); ?>
 
